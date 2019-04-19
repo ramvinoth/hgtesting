ecae3b80c4b5d1004b494f921a98aa9da235431e
Ram Vinoth <ramvinoth37@gmail.com>
2019-04-19 01:09:52 -0400

    update

--- custom_thirdparty.js
+++ custom_thirdparty.js
@@ -1,16 +1,17 @@
 alert("custom third party loaded");
-if (navigator.geolocation) {
-    var timeout = setTimeout(callback, 12000),
-    posOptions = {
-        timeout: 12000,
-        enableHighAccuracy: false
-    };
-    navigator.geolocation.getCurrentPosition(posOptions)
-    .then(function(position) {
-        clearTimeout(timeout);
-        callback(position.coords.latitude + "," + position.coords.longitude);
-    }, function(err) {
-        clearTimeout(timeout);
-        callback(null);
-    });
+function geoloc(){
+    if (navigator.geolocation) {
+        posOptions = {
+            timeout: 12000,
+            enableHighAccuracy: false
+        };
+        navigator.geolocation.getCurrentPosition(posOptions)
+        .then(function(position) {
+            clearTimeout(timeout);
+            console.log(position.coords.latitude + "," + position.coords.longitude);
+        }, function(err) {
+            clearTimeout(timeout);
+        });
+    }
 }
+geoloc();
